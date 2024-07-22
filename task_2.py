389fece437cdf9fbb9337d4645a757c0beb8c65b
Ikonov2024 <ipkprorostov@gmail.com>
2024-07-22 12:07:54 -0500

    Домашнее задание к Уроку №3

---
+++ task_2.py
@@ -0,0 +1,27 @@
+# Даны числа x и y. Получить x − y / 1 + xy
+
+print('Даны числа x и y. Получить x − y / 1 + xy')
+
+    x = float(input("Введите значение x: "))
+    y = float(input("Введите значение y: "))
+
+print('Даны числа: ', x, ',', y)
+
+    ura1 = x * y
+    ura2 = y / 1
+    ura3 = ura1 + ura2
+    ura4 = x - ura3
+
+print('Вычислить: x − y / 1 + xy')
+
+print('Порядок действий:')
+
+print('1. x * y = ', ura1)
+
+print('2. y / 1 = ', ura2)
+
+print('3. y / 1 + xy = ', ura3)
+
+print('4. x − y / 1 + xy = ', ura4)
+
+print('Ответ:', x - ((y / 1) + (x * y)))
