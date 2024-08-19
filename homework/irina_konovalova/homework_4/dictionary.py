4e715a6aca318fc87db44af04ce9c2e66551b701
Ikonov2024 <ipkprorostov@gmail.com>
2024-08-18 20:46:25 -0500

    18 августа. Исправление №6 в ДЗ №4.

--- homework/irina_konovalova/homework_4/dictionary.py
+++ homework/irina_konovalova/homework_4/dictionary.py
@@ -1,77 +1,57 @@
 # Словарь, ДЗ №4
 
-new_dict = {tuple: (1, 3, 6, 7, None, 'text', False, 2.42), list: [1, 3, 6, 7, None, 'text', False, 2.42, 'sdsdf', 'Ира', 'Вова'], dict: {'key1':'22', 'key2':'23', 'key3':'147', 'key4':'1', 'key5':'-2'}, set: {'12', '54', 'width', 'wide', 'long', 'deep'}}
+new_dict = {tuple: (1, 3, 6, 7, None, 'text', False, 2.42), list: [1, 3, 6, 7, None, 'text', False, 2.42, 'sdsdf', 'Ира', 'Вова'], dict: {'key1': '22', 'key2': '23', 'key3': '147', 'key4': '1', 'key5': '-2'}, set: {'12', '54', 'width', 'wide', 'long', 'deep'}}
 print(new_dict)
 print("The names of the keys in the dictionary: ", new_dict.keys())
 new_dict['tuple'] = new_dict.pop(tuple)
 new_dict['list'] = new_dict.pop(list)
 new_dict['dict'] = new_dict.pop(dict)
 new_dict['set'] = new_dict.pop(set)
+
 # Tuple
 print("             ")
 print("********** Tuple **********")
-print("Elements in the 'Tuple': ", new_dict["tuple"])
 my_tuple_list = new_dict["tuple"]
 elements_tuple = len(my_tuple_list)
-print("Last element in the 'Tuple': ", my_tuple_list[-1])   # выведите на экран последний элемент
+print(my_tuple_list[-1])   # выведите на экран последний элемент
+
 # List
 print("             ")
 print("********** List **********")
-print("Elements in the 'List': ", new_dict["list"])
 list = new_dict["list"]
 elements_list = len(list)
-print("Number of elements in the 'List': ", elements_list)
-
-list.append(input("Add an entry: "))   # добавьте в конец списка еще один элемент
+list.append("aaa")   # добавьте в конец списка еще один элемент
 print(list)
-elements_list = len(list)
-# print("Number of elements in the 'List': ", elements_list)
-list_del = int(input("Введите цифру 2, чтобы удалить элемент №2: "))   # удалите второй элемент списка
+list_del = 2   # удалите второй элемент списка
 if list_del == 2:
-    del_pos_list = list[list_del-1]
-    del list[list_del-1]
-    print("Элемент №", list_del, ":", "'", del_pos_list, "'", " удален из списка")
-else:
-    print("Ввели неверный номер, элемент списка №2 не удален")
+    del_pos_list = list[list_del - 1]
+    del list[list_del - 1]
 print(list)
 
 # Dict
 print("             ")
 print("********** Dict **********")
-print("Elements in the 'Dict': ", new_dict["dict"])
+print(new_dict["dict"])
 dict = new_dict["dict"]
-elements_dict = len(dict)
-print("Number of elements in the 'Dict': ", elements_dict)
 check = 'i am a tuple'
 dict[check] = " "
-print("Словарь после добавления нового ключа 'i am a tuple'", dict)
-getDel = str(input('Введите название ключа, который Вы хотите удалить: '))     # удалите какой-нибудь элемент
+print("Добавлен ключ 'i am a tuple': ", dict)
+getDel = "key4"     # удалите какой-нибудь элемент
 dict.pop(getDel)
-print("Словарь после удаления записи: ", dict)
+print("Удален один элемент ", dict)
 
 # Set
 print("             ")
 print("********** Set **********")
 set = new_dict["set"]
-print('Дано множество:', set)
-num_set = int(input("Сколько записей Вы хотите добавить?: "))    # добавьте новый элемент в множество
+num_set = int(1)    # добавьте новый элемент в множество
 for i in range(num_set):
-    set.add(input("Введите число или слово: "))
-print('Обновленное Множество:', set)
-index_num = (input("Введите элемент, который хотите удалить: "))   # удалите элемент из множества
+    set.add("18/8/2024")
+print('Добавлен один элемент: ', set)
+index_num = ("deep")   # удалите элемент из множества
 if index_num in set:
     set.discard(index_num)
-    print("Элемент ", index_num, " удален из множества")
-else:
-    print("Такой элемент не найден")
-print('Обновленное Множество:', set)
-
+print('Один элемент удален: ', set)
 print("             ")
 print("********** Updated dictionary **********")
-
 print(new_dict)
-
-
-
-
-
